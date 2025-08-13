f01e76b4c15b33e5c82d17547a50124b4ae66cdc
ShawnRideout <srideout@asu.edu>
2025-08-13 20:16:29 +0530

    Add .next/static/chunks/src_app_notices_page_tsx_fca1492b._.js 1755096388979

---
+++ .next/static/chunks/src_app_notices_page_tsx_fca1492b._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_notices_page_tsx_bcfbda6b._.js"
+],
+    source: "dynamic"
+});
